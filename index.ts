736c9be7c6d1d08de0beebd9e85f0fd22912db70
ahmdmashod <ahmdmashod@gmail.com>
2025-08-11 18:34:09 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,22 @@
+
+// Store
+const ch = "A";
+const next = " Quick";
+const num = 1;
+const num2 = 45;
+const num5 = -1;
+const old = true;
+
+const personName = "Mashood Ahmad";
+const personAge = 18;
+const alive = true;
+const bankBalance = 5000;
+
+// Action
+console.log(ch + next);
+
+
+// Typescripts Data types
+// Difference Between const & let
+// Console log
+
